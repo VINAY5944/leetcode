f58334b268abc55bb39112b85693e0d10cb74ba3
vinay <vinay5944m@gmail.com>
2024-02-11 21:31:01 +0530

    Time: 83 ms (53.83%) | Memory: 61 MB (5.68%) - LeetSync

---
+++ sql/189-rotate-array/rotate-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    nums.reverse();
+    k = k % nums.length;
+    let last = k - 1;
+    let i = 0;
+    while(i < last) {
+        [nums[i], nums[last]] = [nums[last], nums[i]];
+        last--;
+        i++;
+    }
+    last = nums.length - 1;
+    i = k;
+    while(i < last) {
+        [nums[i], nums[last]] = [nums[last], nums[i]];
+        last--;
+        i++;
+    }
+};
