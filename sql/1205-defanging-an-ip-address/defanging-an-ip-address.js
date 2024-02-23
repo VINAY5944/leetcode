3d79a62a0adf9dd3aa85cb0f2fa0d58cbdc78efa
vinay <vinay5944m@gmail.com>
2024-02-23 20:16:54 +0530

    Time: 53 ms (47.30%) | Memory: 49.2 MB (5.71%) - LeetSync

---
+++ sql/1205-defanging-an-ip-address/defanging-an-ip-address.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    console.log(address.split("."))
+    let arr=address.replaceAll(".","[.]");
+
+return arr
+
+
+
+
+
+};
