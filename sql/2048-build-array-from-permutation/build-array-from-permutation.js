16fa484b2d14c7fc7f044a267d6c38234f1700ba
vinay <vinay5944m@gmail.com>
2024-02-26 19:51:08 +0530

    Time: 65 ms (74.73%) | Memory: 52.4 MB (53.58%) - LeetSync

--- sql/2048-build-array-from-permutation/build-array-from-permutation.js
+++ sql/2048-build-array-from-permutation/build-array-from-permutation.js
@@ -5,11 +5,7 @@
 var buildArray = function(nums) {
     
 
-    let arr=[];
-
-for(let i=0;i<nums.length;i++){
-arr.push(nums[nums[i]])
-
-}
-return arr
+    return nums.map((value, ind) =>{
+        return nums[nums[ind]]
+    })
 };
