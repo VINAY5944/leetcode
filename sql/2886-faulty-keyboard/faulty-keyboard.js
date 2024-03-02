6f5d675e76fb3aebe698eef3832858b0661cdb2a
vinay <vinay5944m@gmail.com>
2024-03-02 22:02:00 +0530

    Time: 104 ms (65.59%) | Memory: 54.6 MB (41.02%) - LeetSync

--- sql/2886-faulty-keyboard/faulty-keyboard.js
+++ sql/2886-faulty-keyboard/faulty-keyboard.js
@@ -3,21 +3,12 @@
  * @return {string}
  */
 var finalString = function(s) {
-    let result = '';
-    let currentString = '';
-    
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === 'i') {
-            // Reverse the current portion of the string typed so far
-            currentString = currentString.split('').reverse().join('');
-        } else {
-            // Append the current character to the string typed so far
-            currentString += s[i];
-        }
+    resultStr = []
+    for(let i=0;i<s.length;i++){
+        if(s[i]==='i')
+            resultStr.reverse()
+        else
+            resultStr.push(s[i])
     }
-    
-    // Append the remaining portion of the string typed so far
-    result += currentString;
-    
-    return result;
+    return resultStr.join('')
 };
