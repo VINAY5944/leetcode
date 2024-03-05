79055d7b22de024d18bc404f73d7eb0051f6806a
vinay <vinay5944m@gmail.com>
2024-03-05 20:10:38 +0530

    Time: 56 ms (61.44%) | Memory: 49.1 MB (9.23%) - LeetSync

---
+++ sql/1013-fibonacci-number/fibonacci-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    let fib = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
+};
