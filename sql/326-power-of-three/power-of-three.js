4de55a90704bc4e870dbfd4b0276d13ebb69d600
vinay <vinay5944m@gmail.com>
2024-03-05 20:59:38 +0530

    Time: 154 ms (70.47%) | Memory: 58.9 MB (37.82%) - LeetSync

---
+++ sql/326-power-of-three/power-of-three.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if (n < 1) {
+        return false;
+    }
+    
+    while (n > 1) {
+        if (n % 3 !== 0) {
+            return false;
+        }
+        n /= 3;
+    }
+    
+    return true;
+};
