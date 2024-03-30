e0b5d17495777d9ebb704159ace978a9886a06b3
vinay <vinay5944m@gmail.com>
2024-03-30 17:13:17 +0530

    Time: 46 ms (88.70%) | Memory: 48.5 MB (84.35%) - LeetSync

---
+++ sql/1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,20 @@
+/**
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let count=0;
+    
+  while(num!==0){
+    if(num%2==0){
+        num=num/2
+        count++
+    }
+    else{
+        num--
+        count++
+    }
+  }
+  return count
+};
