e8345395e98bcd5e0dddba378707db2f3fa1b855
vinay <vinay5944m@gmail.com>
2024-03-30 16:59:37 +0530

    Time: 40 ms (97.99%) | Memory: 48.5 MB (79.42%) - LeetSync

---
+++ sql/1806-count-of-matches-in-tournament/count-of-matches-in-tournament.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function(n) {
+     let count = 0;
+
+    while(n>1){
+
+        count = count + Math.floor(n/2);
+        n = Math.ceil(n/2)
+        
+    }
+
+    return count
+    
+};
