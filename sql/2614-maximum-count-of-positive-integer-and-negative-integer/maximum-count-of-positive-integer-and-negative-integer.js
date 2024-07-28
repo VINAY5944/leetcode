9e89a13d1da7e0ebcc3e309994a00e8e57c70813
vinay <vinay5944m@gmail.com>
2024-07-28 17:43:15 +0530

    Time: 61 ms (34.19%) | Memory: 51.6 MB (27.35%) - LeetSync

--- sql/2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.js
+++ sql/2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.js
@@ -1,25 +1,30 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var maximumCount = function(nums) {
-
-let neg=0;
-let pos=0;
-for(let i=0;i<nums.length;i++){
-
-if(nums[i]<0){
-    neg++
+    let start = 0
+    let end = nums.length - 1
+    let result = -1
+    while (nums[nums.length - 1] === 0) {
+        nums.pop()
+    } 
+    while (start <= end) {
+        let mid = (Math.floor((start + end) / 2))
+        
+        if (nums[mid] > 0) {
+            result = mid
+            end = mid - 1
+        } else {
+            start = mid + 1
+        } 
+    } 
+    if (nums[nums.length - 1] < 0) {
+        return nums.length
+    }
+    if (result === -1) {
+        return 0
+    }
+    let positive = nums.length - result
+    while (nums[result - 1] > -1) {
+        result--
+    }
+    let negative = result
+    return positive > negative ? positive : negative
 }
-
-if(nums[i]>0){
-    pos++
-}
-
-}
-
-
-return neg>pos?neg:pos;
-
-
-};
