5562eddd40ad058ce06330c827dec6de3a08212c
vinay <vinay5944m@gmail.com>
2024-07-30 22:12:23 +0530

    Time: 59 ms (69.45%) | Memory: 50.7 MB (91.65%) - LeetSync

---
+++ sql/3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+
+if(nums[i]<k){
+    count++
+}
+
+else continue
+    }
+    return count
+};
