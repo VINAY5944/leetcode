b58ac4157ffcde4031b870f718abf7f630728de3
vinay <vinay5944m@gmail.com>
2024-08-06 21:19:56 +0530

    Time: 55 ms (28.36%) | Memory: 49.3 MB (38.81%) - LeetSync

---
+++ sql/1289-day-of-the-week/day-of-the-week.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function(day, month, year) {
+    const date = new Date(year, month - 1, day);
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return daysOfWeek[date.getDay()];
+};
+
