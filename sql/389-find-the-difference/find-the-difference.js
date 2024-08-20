b10f48e7fc6ff3d7ad582b55d9a48ca527f8edd7
vinay <vinay5944m@gmail.com>
2024-08-20 19:23:25 +0530

    Time: 68 ms (17.55%) | Memory: 53.4 MB (10.57%) - LeetSync

---
+++ sql/389-find-the-difference/find-the-difference.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    for (let letter of s)
+        t = t.replace(letter, '');
+    return t;
+};
